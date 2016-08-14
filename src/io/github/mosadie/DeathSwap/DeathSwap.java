769b5c995f3c44cbbd908bd8fcc230a7ba76e775
BuildTools <unconfigured@null.spigotmc.org>
2016-08-14 11:19:11 -0700

    Got basic idea written, just need it to actually work...

--- src/io/github/mosadie/DeathSwap/DeathSwap.java
+++ src/io/github/mosadie/DeathSwap/DeathSwap.java
@@ -3,8 +3,15 @@ package io.github.mosadie.DeathSwap;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
 import pro.beam.api.BeamAPI;
 import pro.beam.interactive.net.packet.Protocol;
 import pro.beam.interactive.net.packet.Protocol.Report.TactileInfo;
@@ -15,7 +22,8 @@ public class DeathSwap extends JavaPlugin {
 	public FileConfiguration config = this.getConfig();
 	public BeamAPI beam;
 	public Robot beamRobot;
-	public boolean isGameRunning = false;
+	public Game game;
+	
 	@Override
 	public void onEnable() {
 		config.addDefault("useBeam", false);
@@ -24,6 +32,8 @@ public class DeathSwap extends JavaPlugin {
 		config.addDefault("beam_twofactorrequired", false);
 		config.addDefault("beam_channelid", 1234);
 		config.options().copyDefaults(true);
+		
+		Game game = new Game(this);
 		
 		if (config.getBoolean("useBeam")) {
 			beam = new BeamAPI();
@@ -34,30 +44,43 @@ public class DeathSwap extends JavaPlugin {
 							.password(config.getString("beam_password"))
 							.channel(config.getInt("beam_channelid")).build(beam).get();
 					
-					beamRobot.on(Protocol.Report.class, report -> {
-		                if (report.getTactileCount() > 0) {
-		                    for (int i = 0; i < report.getTactileList().size();i++) {
-		                    	TactileInfo button = report.getTactile(i);
-		                    	switch(button.getId()) {
-		                    	case 0: //Swap Early
-		                    		break;
-		                    	case 1: //Give Lava Bucket
-		                    		break;
-		                    	case 2: //Make Invulnerable
-		                    		break;
-		                    	case 3: //Instant food and hunger refill
-		                    		break;
-		                    	}
-		                    }
-		                }
-		            });
+					startBeamRobot();
 				} catch (InterruptedException | ExecutionException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			} else if (config.getBoolean("beam_twofactorrequired")) {
-				getLogger().warning("Two-Factor Auth enabled! Please run /dsauth <auth code> to complete setup of beam robot!");
+				getLogger().warning("Two-Factor Auth enabled! Please run /ds auth <auth code> to complete setup of beam robot!");
 			}
+		}
+	}
+	
+	public void startBeamRobot() {
+		if (beamRobot != null) {
+			beamRobot.on(Protocol.Report.class, report -> {
+                if (report.getTactileCount() > 0) {
+                    for (int i = 0; i < report.getTactileList().size();i++) {
+                    	TactileInfo button = report.getTactile(i);
+                    	switch(button.getId()) {
+                    	case 0: //Swap Early
+                    		if (game.inGame) swap();
+                    		break;
+                    	case 1: //Give Lava Bucket
+                    		if (game.inGame) giveItem(new ItemStack(Material.LAVA_BUCKET,1));
+                    		break;
+                    	case 2: //Make Invulnerable
+                    		if (game.inGame) givePotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE,60,6));
+                    		break;
+                    	case 3: //Instant food and hunger refill
+                    		if (game.inGame) {
+                    			giveItem(new ItemStack(Material.COOKED_BEEF,64));
+                    			givePotionEffect(new PotionEffect(PotionEffectType.SATURATION,1,10));
+                    		}
+                    		break;
+                    	}
+                    }
+                }
+            });
 		}
 	}
 	
@@ -78,5 +101,60 @@ public class DeathSwap extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	public void swap() {
+		if (game.inGame) {
+		Location player1tmp = game.fighters[0].getLocation();
+		Location player2tmp = game.fighters[1].getLocation();
+		game.fighters[0].teleport(player2tmp);
+		game.fighters[1].teleport(player1tmp);
+		Bukkit.broadcastMessage("SWAPPED!");
+		game.swapTime = 30 + (int)(Math.random() * ((90 - 30) + 1));
+		}
+	}
+
+	public void giveItem(ItemStack itemToGive) {
+		if (game.inGame) {
+			if (game.streamingPlayer != null) {
+				if (game.fighters[0] == game.streamingPlayer | game.fighters[1] == game.streamingPlayer) {
+					game.streamingPlayer.getInventory().addItem(itemToGive);
+					game.streamingPlayer.chat("You have been gifted " + itemToGive.getAmount() + " " + itemToGive.toString() + " by the stream!");
+				}
+				else {
+					game.fighters[0].getInventory().addItem(itemToGive);
+					game.fighters[0].chat("You have been gifted " + itemToGive.getAmount() + " " + itemToGive.toString() + " by the stream!");
+					game.fighters[1].getInventory().addItem(itemToGive);
+					game.fighters[1].chat("You have been gifted " + itemToGive.getAmount() + " " + itemToGive.toString() + " by the stream!");
+				}
+			} else {
+				game.fighters[0].getInventory().addItem(itemToGive);
+				game.fighters[0].chat("You have been gifted " + itemToGive.getAmount() + " " + itemToGive.toString() + " by the stream!");
+				game.fighters[1].getInventory().addItem(itemToGive);
+				game.fighters[1].chat("You have been gifted " + itemToGive.getAmount() + " " + itemToGive.toString() + " by the stream!");
+			}
+		}
+	}
+
+	public void givePotionEffect(PotionEffect effectToGive) {
+		if (game.inGame) {
+			if (game.streamingPlayer != null) {
+				if (game.fighters[0] == game.streamingPlayer | game.fighters[1] == game.streamingPlayer) {
+					game.streamingPlayer.addPotionEffect(effectToGive);
+					game.streamingPlayer.chat("You have been gifted " + effectToGive.toString() + " by the stream!");
+				}
+				else {
+					game.fighters[0].addPotionEffect(effectToGive);
+					game.fighters[0].chat("You have been gifted " + effectToGive.toString() + " by the stream!");
+					game.fighters[1].addPotionEffect(effectToGive);
+					game.fighters[1].chat("You have been gifted " + effectToGive.toString() + " by the stream!");
+				}
+			} else {
+				game.fighters[0].addPotionEffect(effectToGive);
+				game.fighters[0].chat("You have been gifted " + effectToGive.toString() + " by the stream!");
+				game.fighters[1].addPotionEffect(effectToGive);
+				game.fighters[1].chat("You have been gifted " + effectToGive.toString() + " by the stream!");
+			}
+		}
 	}
 }
--- src/io/github/mosadie/DeathSwap/commands/CommandDeathSwap.java
+++ src/io/github/mosadie/DeathSwap/commands/CommandDeathSwap.java
@@ -1,20 +1,68 @@
 package io.github.mosadie.DeathSwap.commands;
 
+import java.util.concurrent.ExecutionException;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import io.github.mosadie.DeathSwap.DeathSwap;
+import pro.beam.interactive.robot.RobotBuilder;
 
 public class CommandDeathSwap implements CommandExecutor{
-
+	private DeathSwap ds;
+	private String[] help = {"How to use the DeathSwap command:","/deathswap start [player1] [player2]", "Use this command to start a game of death swap. If you only have two players on your server, you do not need to include player one or two."," ","/deathswap setstreamer [player]","Used to set the player that is streaming the game on Beam, can be a spectator or a player in the game","","/deathswap auth <6 character code>","Used to enable the Beam robot if the streamer has two-factor auth enabled. Make sure to check the config file.","","/deathswap help","Displays this message."};
+	
+	public CommandDeathSwap(DeathSwap ds) {
+		this.ds = ds;
+	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		if (!sender.isOp()) {
+			sender.sendMessage("You need to be OP to use this command!");
+			return false;
+		}
 		switch (args[0].toLowerCase()) {
 		case "start":
 			//TODO logic to start game
 			return true;
 		case "setstreamer":
-			//TODO Set streaming player
+			if (args[1] == null && sender instanceof Player) {
+				ds.game.streamingPlayer = (Player)sender;
+				return true;
+			} else if (Bukkit.getServer().getPlayer(args[1]) != null) {
+				ds.game.streamingPlayer = Bukkit.getServer().getPlayer(args[1]);
+				return true;
+			}
+			sender.sendMessage("Unable to set steaming player.");
+			return false;
+		case "auth":
+			if (args[1].length() == 6 && ds.beamRobot != null) {
+				ds.getLogger().info("Attempting to use two-factor auth...");
+				try {
+					ds.beamRobot = new RobotBuilder()
+							.username(ds.config.getString("beam_username"))
+							.password(ds.config.getString("beam_password"))
+							.channel(ds.config.getInt("beam_channelid"))
+							.twoFactor(args[1])
+							.build(ds.beam).get();
+					
+					ds.startBeamRobot();
+				} catch (InterruptedException | ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return true;
+			} else {
+				sender.sendMessage("The auth code is not formatted correctly or the Beam Robot already exists.");
+				return false;
+			}
+		case "help":
+			sender.sendMessage(help);
+			return true;
 		default:
+			sender.sendMessage("Unknown command. Try /deathswap help!");
 			return false;
 		}
 	}
